f2541291ec5b6f1f45393822048ba4feca27ee34
wschaeffer <wk98@live.nl>
2020-10-30 21:05:57 +0100

    Clang format

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,21 @@
 /// \brief Most joyful yet useless machine ever!
 
 #include <Arduino.h>
+
 #include "SlowServo.h"
 
-const int DELAY = 8;         //milliseconds
-const long shutdown = 20000; //milliseconds
+const int DELAY     = 8;      // milliseconds
+const long shutdown = 20000;  // milliseconds
 
-const int lidOpen = 10;
+const int lidOpen  = 10;
 const int lidClose = 90;
-const int lidOff = 110;
+const int lidOff   = 110;
 
-const int armPeek = 100;
-const int armClose = 175;
+const int armPeek   = 100;
+const int armClose  = 175;
 const int armSwitch = 75;
 
-const int switchPin = 2;
+const int switchPin   = 2;
 const int lidServoPin = 9;
 const int armServoPin = 10;
 
@@ -25,10 +26,10 @@ SlowServo armServo;
 SlowServo lidServo;
 
 const long shutdownTicks = shutdown / DELAY;
-long shutdownTimer = 0;
+long shutdownTimer       = 0;
 
 bool previousSwitch = true;
-bool switched = false;
+bool switched       = false;
 
 enum class State
 {
@@ -65,20 +66,20 @@ State decision(State a, State b, int weight = 5)
 State handleIdleState(Event event)
 {
   State result = State::IDLE;
-  switch (event)
+  switch(event)
   {
-  case Event::SWITCH:
-    switched = false;
-    shutdownTimer = 0;
-    result = State::OPENING;
-    break;
-  case Event::TICK:
-    shutdownTimer++;
-    if (shutdownTimer >= shutdownTicks)
-    {
-      lidServo.write(lidOff);
-    }
-    break;
+    case Event::SWITCH:
+      switched      = false;
+      shutdownTimer = 0;
+      result        = State::OPENING;
+      break;
+    case Event::TICK:
+      shutdownTimer++;
+      if(shutdownTimer >= shutdownTicks)
+      {
+        lidServo.write(lidOff);
+      }
+      break;
   }
   return result;
 }
@@ -86,18 +87,18 @@ State handleIdleState(Event event)
 State handleOpeningState(Event event)
 {
   State result = State::OPENING;
-  switch (event)
+  switch(event)
   {
-  case Event::TICK:
-    if (lidServo.moveTo(lidOpen) && armServo.moveTo(armClose))
-    {
-      int weight = !switched ? 7 : 2;
-      result = decision(State::PEEKING, State::CLOSING, weight);
-    }
-    break;
-  default:
-    // empty
-    break;
+    case Event::TICK:
+      if(lidServo.moveTo(lidOpen) && armServo.moveTo(armClose))
+      {
+        int weight      = !switched ? 7 : 2;
+        result = decision(State::PEEKING, State::CLOSING, weight);
+      }
+      break;
+    default:
+      // empty
+      break;
   }
   return result;
 }
@@ -105,24 +106,24 @@ State handleOpeningState(Event event)
 State handleClosingState(Event event)
 {
   State result = State::CLOSING;
-  switch (event)
+  switch(event)
   {
-  case Event::SWITCH:
-    result = State::OPENING;
-    break;
-  case Event::TICK:
-    if (lidServo.moveTo(lidClose))
-    {
-      if (switched)
+    case Event::SWITCH:
+      result = State::OPENING;
+      break;
+    case Event::TICK:
+      if(lidServo.moveTo(lidClose))
       {
-        result = decision(State::IDLE, State::OPENING, 8);
+        if(switched)
+        {
+          result = decision(State::IDLE, State::OPENING, 8);
+        }
+        else
+        {
+          result = State::OPENING;
+        }
       }
-      else
-      {
-        result = State::OPENING;
-      }
-    }
-    break;
+      break;
   }
   return result;
 }
@@ -130,24 +131,24 @@ State handleClosingState(Event event)
 State handlePeekingState(Event event)
 {
   State result = State::PEEKING;
-  switch (event)
+  switch(event)
   {
-  case Event::TICK:
-    if (armServo.moveTo(armPeek))
-    {
-      if (!switched)
+    case Event::TICK:
+      if(armServo.moveTo(armPeek))
       {
-        result = decision(State::SWITCHING, State::OPENING, 7);
+        if(!switched)
+        {
+          result = decision(State::SWITCHING, State::OPENING, 7);
+        }
+        else
+        {
+          result = State::OPENING;
+        }
       }
-      else
-      {
-        result = State::OPENING;
-      }
-    }
-    break;
-  default:
-    //empty
-    break;
+      break;
+    default:
+      // empty
+      break;
   }
   return result;
 }
@@ -155,52 +156,52 @@ State handlePeekingState(Event event)
 State handleSwitchingState(Event event)
 {
   State result = State::SWITCHING;
-  switch (event)
+  switch(event)
   {
-  case Event::TICK:
-    if (armServo.moveTo(armSwitch))
-    {
-      switched = true;
-      result = State::OPENING;
-    }
-    break;
-  default:
-    //empty
-    break;
+    case Event::TICK:
+      if(armServo.moveTo(armSwitch))
+      {
+        switched = true;
+        result   = State::OPENING;
+      }
+      break;
+    default:
+      // empty
+      break;
   }
   return result;
 }
 
 void HandleEvent(Event event)
 {
-  switch (currentState)
+  switch(currentState)
   {
-  case State::IDLE:
-    currentState = handleIdleState(event);
-    break;
-  case State::OPENING:
-    currentState = handleOpeningState(event);
-    break;
-  case State::CLOSING:
-    currentState = handleClosingState(event);
-    break;
-  case State::PEEKING:
-    currentState = handlePeekingState(event);
-    break;
-  case State::SWITCHING:
-    currentState = handleSwitchingState(event);
-    break;
+    case State::IDLE:
+      currentState = handleIdleState(event);
+      break;
+    case State::OPENING:
+      currentState = handleOpeningState(event);
+      break;
+    case State::CLOSING:
+      currentState = handleClosingState(event);
+      break;
+    case State::PEEKING:
+      currentState = handlePeekingState(event);
+      break;
+    case State::SWITCHING:
+      currentState = handleSwitchingState(event);
+      break;
   }
 }
 
 void loop()
 {
-  Event event = Event::TICK;
+  Event event        = Event::TICK;
   bool currentSwitch = digitalRead(switchPin);
-  if (currentSwitch != previousSwitch)
+  if(currentSwitch != previousSwitch)
   {
     previousSwitch = currentSwitch;
-    if (!currentSwitch)
+    if(!currentSwitch)
     {
       event = Event::SWITCH;
     }
